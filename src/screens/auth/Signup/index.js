0b9b2351e11cb57e703f61b93b69afeec6e40d35
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-06-25 12:11:14 +0330

    8. Create Checkbox

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { View,SafeAreaView,Text} from 'react-native';
+import React, {useState} from 'react';
+import { View,SafeAreaView,Text, ScrollView} from 'react-native';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
 import styles from './styles';
 import Title from '../../../components/Title';
+import Checkbox from '../../../components/Checkbox';
 
 const Signup = () => {
+    const [agreed, setAgreed] = useState(false);
+
+    const onCheckboxPress = () => {
+      setAgreed(value => !value);
+    };
+
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
         <Title>Join the  hub</Title>
       <Input placeholder="First Name" />
       <Input placeholder="Last Name" />
       <Input placeholder="Email" keyboardType="email-address" />
       <Input placeholder="Password" secureTextEntry />
       <Input placeholder="Confirm Password" secureTextEntry />
+
+        <View>
+            <Checkbox checked={true} onPress={onCheckboxPress}/>
+        </View>
+
        <Button>Create new account</Button>
        <Text style={styles.footerText}>
         Already Registered?
@@ -24,7 +36,7 @@ const Signup = () => {
           Sign in!
         </Text>
       </Text>
-    </SafeAreaView>
+    </ScrollView>
 
   );
 };
