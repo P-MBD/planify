6f585f0416a7ef0ce1eabd08529d6c225845e35b
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-01 09:26:44 +0330

    5. Custom Header

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView,Text, ScrollView} from 'react-native';
 import styles from './styles';
-
+import Header from '../../../components/Header'
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
+        <Header title="Home" />
         <Text>Home</Text>
     </SafeAreaView>
   );
--- src/screens/app/Tasks/index.js
+++ src/screens/app/Tasks/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, ScrollView,Text} from 'react-native';
 import styles from './styles';
+import Header from '../../../components/Header'
 
 const Tasks  = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <Header title="Task"/>
         <Text>Tasks </Text>
     </SafeAreaView>
   );
