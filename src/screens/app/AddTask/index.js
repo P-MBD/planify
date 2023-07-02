76e32b99ce397ce1590f4f8c9fad25e84c5b9489
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-02 11:02:40 +0330

    3. Reuse Categories

--- src/screens/app/AddTask/index.js
+++ src/screens/app/AddTask/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, Pressable, SafeAreaView,Text} from 'react-native';
 import Input from '../../../components/Input';
 import Title from '../../../components/Title';
 import styles from './styles';
-
+import { categories } from '../../../constants/categories';
+import Categories from '../../../components/Categories';
 
 const AddTask  = ({navigation}) => {
+  const [category,setCategory]=useState()
   const handleBack= () =>{
     navigation.goBack()
   }
@@ -18,6 +20,9 @@ const AddTask  = ({navigation}) => {
      <Title type="thin">Add New Task</Title>
      <Text style={styles.label}>Describe the task</Text>
      <Input outlined placeholder="Type here" />
+
+     <Text style={styles.label}>Type</Text>
+     <Categories categories={categories} selectedCategory={category} onCategoryPress={setCategory} />
     </SafeAreaView>
   );
 };
